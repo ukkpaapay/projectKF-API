46d1831717c36ff8b81de013ae599ca2a6856808
ukkpaapay <ukristkrit@hotmail.com>
2026-04-16 17:26:40 +0700

    chore: adjust git workflows

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Local server: http://localhost:${PORT}`);
+    });
+}
 
 export default app;
